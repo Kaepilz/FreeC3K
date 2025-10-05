25281ba939e14591258f8ee114d6a6c8214e0006
mrrwind <mrwind890@gmail.com>
2025-10-05 18:52:48 +0545

    Add files via upload

---
+++ Server/routes/messageRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { protectRoute } from '../middleware/auth.js';
+import { getUsersForSidebar, getMessages, sendMessage, markMessageAsSeen } from '../controllers/messageController.js';
+
+const router = express.Router();
+
+router.get('/users', protectRoute, getUsersForSidebar);
+router.get('/:id', protectRoute, getMessages);
+router.post('/send/:id', protectRoute, sendMessage);
+router.put('/mark/:messageId', protectRoute, markMessageAsSeen);
+
+
+export default router;
